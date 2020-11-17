02bf09c3039837e54ac12712a145797456e14d8f
atik <faisal.atik@gmail.com>
2020-11-17 22:51:38 +0800

    17/11

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import './model/category.dart';
+
+const DUMMY_CATEGORIES = const [
+
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Italian',
+    color: Colors.brown,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Indian',
+    color: Colors.lime,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Japanese',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Chinese',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Mexican',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
